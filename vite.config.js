765667d969918d4e24aaaba284041f5411c235b8
kizeu <116279616+kimzeuner@users.noreply.github.com>
2026-07-06 13:02:17 +0200

    Add Vite configuration for library build

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,20 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/spoolman-filament-card.js",
+      name: "SppolamFilamentCard",
+      formats: ["es"],
+      fileName: () => "spoolman-filament-card.js"
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true
+      }
+    }
+  }
+});
 
